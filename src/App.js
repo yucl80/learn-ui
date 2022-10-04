ccad192080cf7c1cc4f8a16671cb71210118e2b0
yucl <147286318@qq.com>
2022-10-04 18:16:20 +0800

    add data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import * as THREE from "three";
+class App extends Component {
+  componentDidMount() {
+    console.log("componentDidMount");
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    // document.body.appendChild( renderer.domElement );
+    // use ref as a mount point of the Three.js scene instead of the document.body
+    this.mount.appendChild(renderer.domElement);
+    var geometry = new THREE.BoxGeometry(1, 1, 1);
+    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    var cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+    camera.position.z = 5;
+    var animate = function () {
+      requestAnimationFrame(animate);
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      renderer.render(scene, camera);
+    };
+    animate();
+  }
+  render() {
+    return (
+      <div ref={ref => (this.mount = ref)} />
+    )
+  }
 }
+//const rootElement = document.getElementById("root");
+//ReactDOM.render(<App />, rootElement);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+
+//import App from './App';
+//import Test2 from './test2';
 import reportWebVitals from './reportWebVitals';
+//import Test6 from './test6';
+//import MyComponent from './mycomponent';
+import D3Viz from './testd3';
+import D3Viz1 from './testd4';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+//const rootElement = document.getElementById("root");
+
+
 root.render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode>  
+    
+    <D3Viz1/>
   </React.StrictMode>
 );
+
+//root.render(<div><D3Viz1/></div>);
+
+
+console.log("run in index.js")
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
